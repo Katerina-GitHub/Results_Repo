e7d5d49163d8272218044fb63a198c7ca2e04b67
Kate <katia230679@mail.ru>
2022-09-29 11:29:43 +0300

    final task added

---
+++ task/task_final/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arrayOne = new string[8] { "888", "15", "string", "hithere", "bro", "475", "kate", "wohoo" };
+string[] arrayTwo = new string[arrayOne.Length];
+
+void AccessoryArray(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if (arrayOne[i].Length <= 3)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+AccessoryArray(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
